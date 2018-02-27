d412337a052d3c4e7a2a09b36f563e5a0eef51c7
Maksym Bekuzarov <maksym.bekuzarov@nure.ua>
2018-02-27 18:30:28 +0200

    test 5 minutes

--- Bot/Models/Schedulers/PasswordUpdateHostedService.cs
+++ Bot/Models/Schedulers/PasswordUpdateHostedService.cs
@@ -29,6 +29,10 @@ namespace Bot.Models.Schedulers
                 {
                     PasswordProcessorDelegate?.Invoke(passwordUpdateProvider.Password);
                 }
+                else
+                {
+                    PasswordProcessorDelegate?.Invoke("test");
+                }
                 await Task.Delay(TimeSpan.FromMinutes(5), token);
             }
         }
